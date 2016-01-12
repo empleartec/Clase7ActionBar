d49ce4df36873188d43a805b92a8dbec232715af
Gabriel Calero <g@mobaires.com>
2016-01-11 23:28:07 -0300

    ActionViews & ActionProviders

--- app/src/main/java/com/personagraph/clase7actionbar/ActivityCompatActionBar.java
+++ app/src/main/java/com/personagraph/clase7actionbar/ActivityCompatActionBar.java
@@ -1,11 +1,17 @@
 package com.personagraph.clase7actionbar;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class ActivityCompatActionBar extends AppCompatActivity {
 
@@ -24,6 +30,37 @@ public class ActivityCompatActionBar extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(
             R.menu.menu_activity_compat_action_bar, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView =
+                (SearchView) MenuItemCompat.getActionView(searchItem);
+
+        // Configure the search info and add any event listeners...
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // el usuario terminó de ingresar la búsqueda
+                Toast.makeText(ActivityCompatActionBar.this, getString(R.string.searching),
+                        Toast.LENGTH_SHORT).show();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                // el usuario ha tipeado algo
+                return false;
+            }
+        });
+
+
+        MenuItem shareItem = menu.findItem(R.id.action_share);
+        ShareActionProvider myShareActionProvider =
+                (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
+        Intent myShareIntent = new Intent(Intent.ACTION_SEND);
+        myShareIntent.setType("image/*");
+        Uri uri = Uri.parse("android.resource://com.personagraph.clase7actionbar/"+R.drawable.ic_action_search);
+        myShareIntent.putExtra(Intent.EXTRA_STREAM, uri);
+        myShareActionProvider.setShareIntent(myShareIntent);
         return true;
     }
 
